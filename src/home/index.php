987e38df83e199a82ba2098b08ceb628ec45fb7b
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-20 08:48:27 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -6,14 +6,14 @@
     LIVROS TOMBO
     </div>
     <?php
-    $query = "SELECT *, count(*) FROM `am_livros_livro_tombo` group by categoria";
+    $query = "SELECT *, count(*) as quantidade FROM `am_livros_livro_tombo` group by categoria";
     $result = mysqli_query($con, $query);
 
     while($d = mysqli_fetch_object($result)){
     ?>
     <div class="card-body">
         <h5 class="card-title"><?=$d->descricao?></h5>
-        <p class="card-text">Para os libros tombo <?=$d->descricao?> forma detectados <?=$d->quantidade?> de documentos.</p>
+        <p class="card-text">Para os libros tombo <b><?=$d->descricao?></b> forma detectados <b><?=$d->quantidade?></b> de documentos.</p>
     </div>
     <?php
     }
