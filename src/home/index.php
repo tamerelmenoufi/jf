c143e49db926220cc8eb430acad5c3f85fd27ff5
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-20 10:52:32 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -72,7 +72,7 @@
                 ?>
             </ul>
             <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="EstruturaDados" role="tabpanel" aria-labelledby="home-tab" tabindex="0"></div>
+                <div class="tab-pane fade show active m-3" id="EstruturaDados" role="tabpanel" aria-labelledby="home-tab" tabindex="0"></div>
             </div>
         </div>
     </div>
