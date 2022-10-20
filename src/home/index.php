8cf005c9506005423366af28952bc98663ed46db
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-20 10:16:03 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -55,5 +55,39 @@
         </div>
 
 
+
+
+
+
+
+
+        <!-- TÍTULOS PLOTADOS -->
+        <div class="col-12 m-3">
+            <div class="card">
+                <div class="card-header">
+                Títulos Plotados
+                </div>
+                <?php
+                $query = "SELECT *, count(*), replace(descricao,SUBSTR(descricao, -4),'') as desci FROM `cartografia_titulos_plotados` group by desci";
+                $result = mysqli_query($con, $query);
+
+                while($d = mysqli_fetch_object($result)){
+                    // $descricao = str_replace(array('-','_','.pdf'), " ",$d->descricao);
+                    $descricao = explode(".",$d->descricao);
+                    $descricao = $descricao[0];
+                ?>
+                <div class="card-body">
+                    <h5 class="card-title"><?=$d->tipo?></h5>
+                    <p class="card-text">Forma detectados estrutura de arquivos SHAPEFILE para <b><?=strtoupper($d->desci)?></b>.</p>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+
+
+
+
     </div>
 </div>
