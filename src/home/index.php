0758cf89d0dbcdb95cce68a98a3c2e418755ceb2
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-20 08:44:51 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -1,9 +1,22 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/jf/lib/includes.php");
-    exit();
-    $query = "select * from dados limit 100";
+?>
+<div class="card">
+    <div class="card-header">
+    LIVROS TOMBO
+    </div>
+    <?php
+    $query = "SELECT *, count(*) FROM `am_livros_livro_tombo` group by categoria";
     $result = mysqli_query($con, $query);
 
     while($d = mysqli_fetch_object($result)){
-        echo $d->url."<br>";
+    ?>
+    <div class="card-body">
+        <h5 class="card-title"><?=$d->descricao?></h5>
+        <p class="card-text">Para os libros tombo <?=$d->descricao?> forma detectados <?=$d->quantidade?> de documentos.</p>
+    </div>
+    <?php
     }
+    ?>
+</div>
+
