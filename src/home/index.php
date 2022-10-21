6c05816e34e6aa2e7a9e4477ae0116734753d235
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-21 08:51:03 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -13,24 +13,27 @@
 
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <a class="navbar-brand" href="#">CONSULTORIA</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Resumo do projeto</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Plataformas de Gestão</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Banco de Dados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PostGis</a>
         </li>
       </ul>
       <span class="navbar-text">
-        Navbar text with an inline element
+        Administrador do Sistema
       </span>
     </div>
   </div>
