fc705e0482e4839212cd3b4dd99d17d275dfd044
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-21 08:17:16 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -93,12 +93,12 @@
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-6" style="text-align:center;">
-                                <a href='http://consultoria.mohatron.com:8000/' target='_blank'><img src="img/phpmyadmin.png" style="width:90%; background-color:#eee; border-radius:7px; cursor:pointer;" /></a>
+                            <div class="col-md-6" style="text-align:center; background-color:#eee; border-radius:7px; padding:10px;">
+                                <a href='http://consultoria.mohatron.com:8000/' target='_blank'><img src="img/phpmyadmin.png" style="width:90%; cursor:pointer;" /></a>
                                 <p style="margin-top:10px;">Gerenciandor do banco de dados e estrutura de informações do sistema. Acesse <a href='http://consultoria.mohatron.com:8000/' target='_blank'>PhpMyadmin</a></p>
                             </div>
-                            <div class="col-md-6" style="text-align:center;">
-                                <a href='http://consultoria.mohatron.com:8083/' target='_blank'><img src="img/pgadmin.png" style="width:90%; background-color:#eee; border-radius:7px; cursor:pointer;" /></a>
+                            <div class="col-md-6" style="text-align:center; background-color:#eee; border-radius:7px; padding:10px;">
+                                <a href='http://consultoria.mohatron.com:8083/' target='_blank'><img src="img/pgadmin.png" style="width:90%; cursor:pointer;" /></a>
                                 <p style="margin-top:10px;">Gerenciandor do banco de dados Geográficos e informações relacionadas. Acesse <a href='http://consultoria.mohatron.com:8083/' target='_blank'>PgAdmin</a></p>
                             </div>
                         </div>
