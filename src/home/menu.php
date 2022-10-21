1e858169a2b025dce77e991494ed8ecc6f784445
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-21 09:16:12 -0400

    Atualizações

--- src/home/menu.php
+++ src/home/menu.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CONSULTORIA</a>
+    <span class="navbar-brand" href="#">CONSULTORIA</span>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" url="src/home/index.php">Resumo do projeto</a>
+          <span class="nav-link" url="src/home/index.php">Resumo do projeto</span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" url="src/home/plataforma_gestao.php">Plataformas de Gestão</a>
+          <span class="nav-link" url="src/home/plataforma_gestao.php">Plataformas de Gestão</span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" url="src/home/estrutura_bd.php">Banco de Dados</a>
+          <span class="nav-link" url="src/home/estrutura_bd.php">Banco de Dados</span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" url="src/home/estrutura_gis.php">PostGis</a>
+          <span class="nav-link" url="src/home/estrutura_gis.php">PostGis</span>
         </li>
       </ul>
       <span class="navbar-text">
