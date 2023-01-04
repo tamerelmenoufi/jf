f30963cf6c8f05292ffaeb97ef71d58eff6994e8
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 10:48:17 -0400

    Atualizações

--- src/processos/lista_processos.php
+++ src/processos/lista_processos.php
@@ -4,6 +4,15 @@
 
     $query = "select * from processos where {$_POST['campo']} LIKE '%{$_POST['busca']}%' limit 100";
     $result = mysqli_query($con, $query);
+    $n = mysqli_num_rows($result);
+    if(!$n){
+?>
+<div class="d-flex align-content-center h-100">
+    <h1 style="color:#a1a1a1" >Nenhum resultado para<br><?="{$_POST['busca']}"?></h1>
+</div>
+<?php
+    }
+
     while($d = mysqli_fetch_object($result)){
 ?>
 
