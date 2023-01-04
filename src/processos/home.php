e7b03e436c16829233ecd792ac2357e4e1807a4f
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 11:38:13 -0400

    Atualizações

--- src/processos/home.php
+++ src/processos/home.php
@@ -11,7 +11,7 @@
     }
 </style>
 
-<ul class="nav nav-tabs" id="myTab" role="tablist" style="margin-top:50px;">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
     <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
   </li>
