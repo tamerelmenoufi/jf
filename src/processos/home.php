820eabbb4c02b62ddbb570fcaf45d1c6aaa00e72
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 11:29:31 -0400

    Atualizações

--- src/processos/home.php
+++ src/processos/home.php
@@ -1,7 +1,7 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/jf/lib/includes.php");
 ?>
-<div class="m-3">
+<div class="m-3" style="margin-top:60px">
     <ul class="nav nav-tabs">
     <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#">Active</a>
