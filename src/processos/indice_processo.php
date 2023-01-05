51fedbe2f8c794990042704df6774af4b3badfc1
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 21:47:17 -0400

    Atualizações

--- src/processos/indice_processo.php
+++ src/processos/indice_processo.php
@@ -11,6 +11,6 @@
     <a href="http://moh1.com.br/jf/SIGLO_AM/CONVENIO 2000\08-Titulos Plotados\AM\01-Livros\03-Indice Remissivo de Processo/ÍNDICE REMISSIVO DE PROCESSOS.pdf" target="_blank"><i class="fa-regular fa-file-pdf"></i></a>
   </li>
   <li class="list-group-item d-flex justify-content-between h-100">
-    <object data="<?=$urlData.substr($d->url,1,strlen($d->url))?>" type="aplication/pdf" class="h-100 w-100" ></object>
+    <object data="<?=$urlData.substr($d->url,1,strlen($d->url))?>" type="" class="h-100 w-100" ></object>
   </li>
 </ul>
