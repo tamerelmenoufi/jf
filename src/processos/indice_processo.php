24a5eca7497ffbd06890ce2e3a6e4a6ab41b0e78
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 21:30:43 -0400

    Atualizações

--- src/processos/indice_processo.php
+++ src/processos/indice_processo.php
@@ -1,9 +1,13 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/jf/lib/includes.php");
+
+    $query = "select * from processos where codigo = '{$_POST['cod']}'";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
 ?>
-<ol class="list-group list-group-numbered">
+<ul class="list-group list-group-numbered">
   <li class="list-group-item d-flex justify-content-between align-items-start">
     <span>Registro do índice do processo</span>
     <a href="http://moh1.com.br/jf/SIGLO_AM/CONVENIO 2000\08-Titulos Plotados\AM\01-Livros\03-Indice Remissivo de Processo/ÍNDICE REMISSIVO DE PROCESSOS.pdf" target="_blank"><i class="fa-regular fa-file-pdf"></i></a>
   </li>
-</ol>
+</ul>
