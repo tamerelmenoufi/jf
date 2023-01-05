724c46810cf7db0aea8a572aa24f550c69a206f0
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-04 21:34:21 -0400

    Atualizações

--- src/processos/indice_processo.php
+++ src/processos/indice_processo.php
@@ -5,7 +5,7 @@
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 ?>
-<ul class="list-group list-group-numbered">
+<ul class="list-group">
   <li class="list-group-item d-flex justify-content-between">
     <span>Registro do índice do processo</span>
     <a href="http://moh1.com.br/jf/SIGLO_AM/CONVENIO 2000\08-Titulos Plotados\AM\01-Livros\03-Indice Remissivo de Processo/ÍNDICE REMISSIVO DE PROCESSOS.pdf" target="_blank"><i class="fa-regular fa-file-pdf"></i></a>
